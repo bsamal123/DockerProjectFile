80da9dd1da0e9d5f3ef85c6656481714bb431ba9
Biswa <biswasamal2003@gmail.com>
2025-02-19 17:58:58 -0600

    added gain

--- Test.java
+++ Test.java
@@ -4,7 +4,13 @@ class HelloWorld{
         int b=20;
         int sum=a+b;
         System.out.println("The sum is now "+sum);
-        System.out.println("The sum is     tada"+sum);  
+        System.out.println("The sum is   tada"+sum);   
+        System.out.println("The sum is now "+sum);
+        System.out.println("The sum is   tada"+sum); 
+        System.out.println("The sum is now "+sum);
+        System.out.println("The sum is   tada"+sum);  
+        System.out.println("The sum is now "+sum);
+        System.out.println("The sum is   tada"+sum);  
 }
     
     }
